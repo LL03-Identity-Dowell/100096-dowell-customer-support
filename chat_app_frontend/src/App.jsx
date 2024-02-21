829d72323a13779020d664611e27e355a4a195db
melese <meleseayen1999@gmail.com>
2024-02-21 17:42:30 +0300

    Routing is added

--- chat_app_frontend/src/App.jsx
+++ chat_app_frontend/src/App.jsx
@@ -1,10 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import CreateTicket from "./components/CreateTicket";
+import SigninForm from "./components/shared/SigninForm";
 
 export default function App() {
   return (
     <>
       <div>
-        <CreateTicket />
+        <Routes>
+          {/* Public Routes */}
+          <Route index element={<CreateTicket />} />
+          <Route path="/sign-in" element={<SigninForm />} />
+        </Routes>
       </div>
     </>
   );
--- chat_app_frontend/src/components/CreateTicket.jsx
+++ chat_app_frontend/src/components/CreateTicket.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+import SearchComponent from "./shared/SearchComponent";
 import TicketLogo from "./shared/TicketLogo";
 import TicketMainContent from "./shared/TicketMainContent";
 import TicketTopNav from "./shared/TicketTopNav";
 
 const CreateTicket = () => {
+  const [isSearchModalOpen, setIsSearchModalOpen] = useState(false);
+
+  const openSearchModal = () => {
+    setIsSearchModalOpen(true);
+  };
+
+  const closeSearchModal = () => {
+    setIsSearchModalOpen(false);
+  };
+
   return (
     <section>
-      <TicketTopNav />
+      {isSearchModalOpen && (
+        <SearchComponent closeSearchModal={closeSearchModal} />
+      )}
+      <TicketTopNav openSearchModal={openSearchModal} />
       <div className="main_cont">
         <TicketLogo />
         <TicketMainContent />
