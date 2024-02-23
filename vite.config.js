ac22fbf4153053e2a838b32b4ec7eb5eb0814d81
melese <meleseayen1999@gmail.com>
2024-02-23 08:39:03 +0300

    Deploy data added

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/chat_app_frontend/",
+  base: "/100096-customer-support",
 });
