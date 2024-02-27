a65911c5a0d96d495c4866c0d9af0a9060ef9f30
abatetsegaye1 <abatetsegaye12@gmail.com>
2024-02-27 11:07:34 +0300

    merged

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { store } from "./Redux/store.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider basename="/service" router={router} />
+      <RouterProvider basename="/100096-customer-support" router={router} />
     </Provider>
   </React.StrictMode>
 );
