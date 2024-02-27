32354c2f1c990d44b2d6a5667b3a434ffe6c36f7
abatetsegaye1 <abatetsegaye12@gmail.com>
2024-02-27 10:54:51 +0300

    change in config

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { store } from "./Redux/store.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <RouterProvider basename="/service" router={router} />
     </Provider>
   </React.StrictMode>
 );
