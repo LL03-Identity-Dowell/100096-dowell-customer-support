48fb6d408fc77ee4c1c817a9cd72dcbe1f2cf644
abatetsegaye1 <abatetsegaye12@gmail.com>
2024-02-29 21:02:53 +0300

    file changed 10

--- src/linemanage/main.jsx
+++ src/linemanage/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router.jsx";
 import { Provider } from "react-redux";
 import { store } from "./Redux/store.js";
+import { root } from "../main.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
