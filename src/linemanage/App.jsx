f7d2148a39ef76672d3afa8b78ee16ef25ef1df2
abatetsegaye1 <abatetsegaye12@gmail.com>
2024-02-29 18:41:49 +0300

    file changed2

---
+++ src/linemanage/App.jsx
@@ -0,0 +1,13 @@
+import Dashboard from "./components/Dashboard";
+import NavItem from "./components/NavItem";
+import "./index.css";
+function App() {
+  return (
+    <>
+      <NavItem />
+      <Dashboard />;
+    </>
+  );
+}
+
+export default App;
