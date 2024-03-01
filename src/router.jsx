bf003bcad7cf3b21770ebade07d250b8c5bb6e61
abatetsegaye1 <abatetsegaye12@gmail.com>
2024-03-01 08:40:31 +0300

    added

--- src/router.jsx
+++ src/router.jsx
@@ -1,12 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import TicketDetail from "./linemanage/components/TicketDetail";
 import AppLine from "./linemanage/App";
-import App from "./App";
-const basePath = "/100096-dowell-customer-support/";
+import SigninForm from "./components/shared/SigninForm";
+import CreateTicket from "./components/CreateTicket";
+const basePath = "/100096-dowell-customer-support/linemanage";
 const router = createBrowserRouter([
   {
-    path: `/100096-dowell-customer-support/linemanage`,
-    element: <App />,
+    path: `/100096-dowell-customer-support/`,
+    element: <CreateTicket />,
+    children: [
+      {
+        path: "/100096-dowell-customer-support/sign-in",
+        element: <SigninForm />,
+      },
+    ],
   },
   {
     path: `${basePath}`,
