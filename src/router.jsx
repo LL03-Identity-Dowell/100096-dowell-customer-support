d06d2f36774427b3df5d6999cca526625f71fb90
Githaiga <pythongeeko@gmail.com>
2024-03-06 11:53:07 +0300

    deploy to vps 🚀⛴

--- src/router.jsx
+++ src/router.jsx
@@ -3,28 +3,29 @@ import TicketDetail from "./linemanage/components/TicketDetail";
 import AppLine from "./linemanage/App";
 import SigninForm from "./components/shared/SigninForm";
 import CreateTicket from "./components/CreateTicket";
-const basePath = "/100096-dowell-customer-support";
+
+
 const router = createBrowserRouter([
   {
     path: basePath,
     element: <CreateTicket />,
     children: [
       {
-        path: `${basePath}/sign-in`,
+        path: `/sign-in`,
         element: <SigninForm />,
       },
     ],
   },
   {
-    path: `${basePath}/linemanage`,
+    path: `/linemanage`,
     element: <AppLine />,
     children: [
       {
-        path: `${basePath}/linemanage`,
+        path: `/linemanage`,
         element: <TicketDetail />,
       },
       {
-        path: `${basePath}/linemanage/ticketDetail/:ticketID`,
+        path: `/linemanage/ticketDetail/:ticketID`,
         element: <TicketDetail />,
       },
     ],
