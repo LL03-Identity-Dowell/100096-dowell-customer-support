46bfd7514650b94d5251f1b4620f47702faf4f61
melese-2121 <meleseayen2016@gmail.com>
2024-05-01 10:27:19 +0300

    Create Ticket UI Edited

--- src/components/shared/ChatForm.jsx
+++ src/components/shared/ChatForm.jsx
@@ -66,20 +66,20 @@ const ChatForm = ({
   return (
     <div
       ref={containerRef}
-      className={`fixed flex left-1/2 transform -translate-x-1/2  -translate-y-1/2 w-[38%] max-w-[60%] min-w-[360px] min-h-[85%] max-h-[95%] ${
-        darkMode ? "bg-gray-800" : "bg-neutral-400"
+      className={`fixed flex left-1/2 transform -translate-x-1/2  -translate-y-1/2 w-[80%] max-w-[100%] min-w-[360px] min-h-[85%] max-h-[95%] ${
+        darkMode ? "bg-gray-600" : "bg-white"
       } rounded-lg shadow-lg z-10 top-[50%] duration-1000`}
     >
-      <div className={`p-1 pb-0 w-full `}>
-        <div className="flex justify-between">
+      <div className={` pb-0 w-full `}>
+        <div className="flex justify-between #22C55E rounded-t-md py-3 px-5 bg-[#22C55E]">
           <h2
-            className={`text-lg ${
+            className={`text-xl ${
               darkMode ? "text-white" : "text-slate-900"
             } font-semibold p-2 mb-2`}
           >
             {JSON.parse(localStorage.getItem("create_ticket_detail"))._id}
           </h2>
-          <div className="flex justify-end items-end overflow-hidden h-10 min-w-20  pr-1">
+          <div className="flex justify-end items-end gap-2 overflow-hidden h-10 min-w-20  pr-1">
             <Toggler darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
             <button
               className="text-red-600 font-extrabold rounded-full h-10 w-10 text-center px-auto  transition-all duration-1000 hover:bg-slate-700"
@@ -103,12 +103,12 @@ const ChatForm = ({
           </div>
         </div>
 
-        <div className=" p-4 rounded-lg shadow-lg overflow-y-auto h-[79%]  pb-10 w-full ">
+        <div className=" p-4 md:px-10 rounded-lg  overflow-y-auto h-[79%]  pb-10 w-full ">
           <div
             ref={containerRef}
             className="custom-scrollbar space-y-4 pl-1  -pr-1 pb-5  w-full "
           >
-            {!messageToDisplay && (
+            {/* {!messageToDisplay && (
               <div className="flex flex-col justify-center mt-10 items-center text-center w-full h-full">
                 <h1 className="text-white text-4xl font-bold font-mono mb-5">
                   Need help?
@@ -118,7 +118,7 @@ const ChatForm = ({
                   Ask Us
                 </h1>
               </div>
-            )}
+            )} */}
             {messageToDisplay.length > 0 &&
               messageToDisplay.map((message) => (
                 <div
@@ -172,12 +172,10 @@ const ChatForm = ({
               ))}
           </div>
         </div>
+        <hr className="py-5 md:w-[85%] text-center mx-auto -mt-10" />
+
         <form onSubmit={(e) => handleSend(e)}>
-          <div
-            className={`pt-5 px-3 ${
-              darkMode ? "bg-gray-800 " : "bg-neutral-400"
-            } duration-1000`}
-          >
+          <div className={`-mt-3 md:mx-[5%] px-5  duration-1000  `}>
             <div
               className="flex gap-x-2"
               style={{
@@ -188,18 +186,18 @@ const ChatForm = ({
             >
               <input
                 id="message"
-                className="w-full px-3 py-2 bg-gray-200 rounded-lg  focus:ring-2 focus:border-blue-400 focus:outline-none"
+                className="w-full md:mx-3 px-3 py-2  rounded-lg  focus:ring-2 focus:border-blue-400 focus:outline-none"
                 type="text"
                 placeholder="Type your message..."
                 value={message}
                 onChange={handleMessageChange}
               />
               <button
-                className="text-white rounded-lg w-[15%]  flex items-center justify-center bg-slate-700 hover:bg-slate-800 transition-delay-1000"
+                className="text-white   flex items-center justify-center transition-delay-1000"
                 type="submit"
               >
                 <FontAwesomeIcon
-                  className="mx-2 w-8 h-8 text-green-500 "
+                  className="mx-2 w-10 h-10  text-green-500 hover:text-green-300 duration-500  "
                   icon={faTelegramPlane}
                 />
               </button>
--- src/linemanage/components/NavItem.jsx
+++ src/linemanage/components/NavItem.jsx
@@ -8,21 +8,21 @@ export default function NavItem({ component }) {
     window.location.replace(dowellLogoutUrl);
   };
   return (
-    <div className="flex md:flex-row ">
-      <div className="md:flex md:flex-row h-300 w-full sm:flex-row flex-2">
-        <a className="flex items-center ps-2.5 md:mb-2 sm:mb-2 w-[25%] h-16">
+    <div className="flex justify-start bg-[#22C55E] bg-opacity-[9%] items-center text-center">
+      <div className="flex justify-start  w-full flex-2">
+        <a className="flex justify-start  items-start ps-2.5 md:mb-2 sm:mb-2 md:mr-2 h-16">
           <img
             src="https://psp-logos.uptimerobot.com/logos/2021049-1676548510.png"
-            className="md:h-full md:w-[25%] mx-auto sm:w-[10%] rounded-md border-none"
+            className="md:w-16 md:h-16  md:ml-5 rounded-md border-none"
             alt="Flowbite Logo"
           />
         </a>
-        <h2 className="text-lg text-center justify-self-center self-center">
+        <h2 className="text-lg font-bold text-center max-md:hidden text-[#22C55E] flex justify-center items-center  ">
           Customer Support | Line Manager
         </h2>
         {/* <hr className="border-b-1 bg-slate-500 mx-auto w-[90%]" /> */}
       </div>
-      {!component && (
+      {/* {!component && (
         <div className="flex-1 mr-3">
           <button
             onClick={handleLogout}
@@ -31,7 +31,11 @@ export default function NavItem({ component }) {
             Logout
           </button>
         </div>
-      )}
+      )} */}
+
+      {/* <div>
+        <FaUser />
+      </div> */}
     </div>
   );
 }
