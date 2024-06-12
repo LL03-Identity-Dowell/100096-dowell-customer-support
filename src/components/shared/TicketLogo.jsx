45e337bc614075b82355e0d805ad980e864181d0
melese-2121 <meleseayen2016@gmail.com>
2024-06-12 05:19:20 +0300

    For mobile fixed

--- src/components/shared/TicketLogo.jsx
+++ src/components/shared/TicketLogo.jsx
@@ -2,11 +2,11 @@ import { logo2 } from "../../assets";
 
 const TicketLogo = () => {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center max-md:-mt-5">
       <img
         src={logo2}
         alt="Dowell Logo"
-        className=" h-[170px]  max-md:w-[200px] md:w-[250px] max-sm:-mt-5  sm:-pt-16"
+        className=" h-auto   max-md:w-[150px] md:w-[250px] max-sm:-mt-5   sm:-pt-16"
       />
     </div>
   );
