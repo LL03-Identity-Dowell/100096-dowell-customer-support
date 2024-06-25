7c517132a0b7622fb4badab4ce9c9a3ad12d2bef
abatetsegaye1 <abatetsegaye12@gmail.com>
2024-06-25 09:51:28 +0300

    change in master link

--- src/linemanage/components/CreateComponent.jsx
+++ src/linemanage/components/CreateComponent.jsx
@@ -14,7 +14,7 @@ import io from "socket.io-client";
 import { ManagerNavBar } from "./ManagerNavBar";
 import ListData from "./ListData";
 import ListTopic from "./ListTopic";
-import ListLinks from "./ListLinks";
+// import ListLinks from "./ListLinks";
 
 const socket = io.connect("https://www.dowellchat.uxlivinglab.online/");
 
@@ -450,12 +450,12 @@ function CreateComponent({ closeSearchModal, option }) {
 
             {option === "createLink" && tab === "viewLink" && (
               <div className={`min-h-[80%]`}>
-                <ListLinks search={search} />
+                {/* <ListLinks search={search} /> */}
               </div>
             )}
             {option === "createLink" && tab === "" && (
               <div className={`min-h-[80%]`}>
-                <ListLinks search={search} />
+                {/* <ListLinks search={search} /> */}
               </div>
             )}
 
